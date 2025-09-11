7b1722a86a268d14ea040d024bd554fc8eb20bf7
sparsh-15 <sparshsahu8435@gmail.com>
2025-09-11 17:02:33 +0530

    UserService completed

--- backend/src/main/java/com/erp/crm/services/UserService.java
+++ backend/src/main/java/com/erp/crm/services/UserService.java
@@ -1,5 +1,9 @@
 package com.erp.crm.services;
 
+import java.util.Optional;
+
+import com.erp.crm.models.Role;
+import com.erp.crm.models.User;
 import com.erp.crm.repositories.RoleRepository;
 import com.erp.crm.repositories.UserRepository;
 
@@ -11,4 +15,16 @@ public class UserService {
         this.userRepo = userRepo;
         this.roleRepo = roleRepo;
     }
+
+    public User createUser(User user) {
+        return userRepo.save(user);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepo.findByEmail(email);
+    }
+
+    public Optional<Role> getRoleByName(String roleName) {
+        return roleRepo.findByName(roleName);
+    } 
 }
