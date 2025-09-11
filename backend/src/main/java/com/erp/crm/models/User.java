bfbd8e60e83019e2995687e86fc57a1fdc8313f9
sparsh-15 <sparshsahu8435@gmail.com>
2025-09-11 16:36:45 +0530

    User Model

--- backend/src/main/java/com/erp/crm/models/User.java
+++ backend/src/main/java/com/erp/crm/models/User.java
@@ -1,10 +1,18 @@
 package com.erp.crm.models;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,7 +24,25 @@ public class User {
     @Column(name = "user_id")
     private Integer userId;
 
-    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id",nullable = false)
+    private Role role;
 
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String phone;
+    private Boolean isActive = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;  
 
 }
