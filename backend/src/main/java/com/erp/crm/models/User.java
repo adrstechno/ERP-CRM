8c15da4ec2a047a96cf6d894ddbe8c9b0e94addd
sparsh-15 <sparshsahu8435@gmail.com>
2025-09-11 16:47:11 +0530

    update getter setter

--- backend/src/main/java/com/erp/crm/models/User.java
+++ backend/src/main/java/com/erp/crm/models/User.java
@@ -14,9 +14,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
