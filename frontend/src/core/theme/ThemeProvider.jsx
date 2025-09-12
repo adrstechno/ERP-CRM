ddf087d6380c65c41b1c8259d1369439b8c55eca
Darkness1805 <139411743+Darkness1805@users.noreply.github.com>
2025-09-12 11:30:08 +0530

    Update ThemeProvider.jsx

--- frontend/src/core/theme/ThemeProvider.jsx
+++ frontend/src/core/theme/ThemeProvider.jsx
@@ -8,27 +8,16 @@ export const useThemeMode = () => useContext(ThemeModeContext);
 export { useTheme };
 
 export default function ThemeProvider({ children }) {
-<<<<<<< HEAD
   const [mode, setMode] = useState("dark");
 
   const toggleMode = () => {
     setMode((prev) => (prev === "dark" ? "light" : "dark"));
-=======
-  const [mode, setMode] = useState("light");
-
-  const toggleMode = () => {
-    setMode((prev) => (prev === "light" ? "dark" : "light"));
->>>>>>> 07bf7762e445d1fa0158ee87f3da9e91015134ee
   };
 
   const muiTheme = useMemo(() => theme(mode), [mode]);
 
   return (
-<<<<<<< HEAD
     <ThemeModeContext.Provider value={{ mode, toggleMode }}>
-=======
-    <ThemeModeContext.Provider value={{ toggleMode }}>
->>>>>>> 07bf7762e445d1fa0158ee87f3da9e91015134ee
       <MuiThemeProvider theme={muiTheme}>
         <CssBaseline />
         {children}
