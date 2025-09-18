9189b2c8ca9eb8a2b4b5a6cfa9daac8b3cea89e8
sparsh-15 <sparshsahu8435@gmail.com>
2025-09-18 16:03:50 +0530

    added dealer repo

---
+++ backend/src/main/java/com/erp/crm/repositories/DealerRepository.java
@@ -0,0 +1,13 @@
+package com.erp.crm.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.erp.crm.models.DealerProfile;
+
+public interface DealerRepository extends JpaRepository<DealerProfile, Long> {
+    Optional<DealerProfile> findByUserId(Long userId);
+
+    
+}
