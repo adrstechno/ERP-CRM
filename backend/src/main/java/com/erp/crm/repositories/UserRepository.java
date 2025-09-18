6334b932ddd26fc9a22abd566be3cb954f4866b0
sparsh-15 <sparshsahu8435@gmail.com>
2025-09-18 16:03:57 +0530

    added userrespo

--- backend/src/main/java/com/erp/crm/repositories/UserRepository.java
+++ backend/src/main/java/com/erp/crm/repositories/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.erp.crm.models.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
 }
