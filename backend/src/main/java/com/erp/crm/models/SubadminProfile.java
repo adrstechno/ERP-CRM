9dad952f26f58d05db19295a842244d59141d2c1
sparsh-15 <sparshsahu8435@gmail.com>
2025-09-18 16:08:32 +0530

    subadmin getSEtss

--- backend/src/main/java/com/erp/crm/models/SubadminProfile.java
+++ backend/src/main/java/com/erp/crm/models/SubadminProfile.java
@@ -8,9 +8,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "subadmin_profiles")
+@Getter
+@Setter
 public class SubadminProfile {
 
     @Id
