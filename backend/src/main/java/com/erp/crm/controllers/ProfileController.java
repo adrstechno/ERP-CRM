76044fdf47be67ab59ccbb5502c63858649e746f
sparsh-15 <sparshsahu8435@gmail.com>
2025-09-22 11:00:52 +0530

    updated

--- backend/src/main/java/com/erp/crm/controllers/ProfileController.java
+++ backend/src/main/java/com/erp/crm/controllers/ProfileController.java
@@ -17,7 +17,6 @@ import com.erp.crm.services.DealerProfileService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RequestMapping("/api/profiles")
