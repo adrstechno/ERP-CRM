a69c33c0ecad68162e26b6e0c1b1f79ea08a37cc
SakHack <sakshi2408jain@gmail.com>
2025-10-06 11:18:58 +0530

    getter setter

--- backend/src/main/java/com/erp/crm/dto/SaleItemDto.java
+++ backend/src/main/java/com/erp/crm/dto/SaleItemDto.java
@@ -2,11 +2,15 @@ package com.erp.crm.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class SaleItemDTO {
     private Long productId;   // ✅ Use ID instead of name
     private String productName; // ✅ Filled only in response
