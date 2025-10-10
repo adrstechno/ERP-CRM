1a3a2ac5fb5d134cc5efe929679fe7e8faf1f504
sparsh-15 <sparshsahu8435@gmail.com>
2025-10-10 10:48:47 +0530

    fix saleRepo  custom methods

--- backend/src/main/java/com/erp/crm/repositories/SaleRepository.java
+++ backend/src/main/java/com/erp/crm/repositories/SaleRepository.java
@@ -6,12 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.erp.crm.models.Sale;
-import com.erp.crm.models.User;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
-    List<Sale> findAllByDealer(User dealer);
-    List<Sale> findAllByMarketer(User marketer);
+List<Sale> findAllByCreatedByUserId(Long userId);
 
 }
