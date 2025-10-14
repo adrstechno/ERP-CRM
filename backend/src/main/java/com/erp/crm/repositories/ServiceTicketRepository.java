d264000b75630536b06ead1f7a60471655521b7e
SakHack <sakshi2408jain@gmail.com>
2025-10-14 13:56:01 +0530

    added some methods

--- backend/src/main/java/com/erp/crm/repositories/ServiceTicketRepository.java
+++ backend/src/main/java/com/erp/crm/repositories/ServiceTicketRepository.java
@@ -1,9 +1,13 @@
 package com.erp.crm.repositories;
 
 import com.erp.crm.models.ServiceTicket;
+import com.erp.crm.models.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ServiceTicketRepository extends JpaRepository<ServiceTicket, Long> {
+
+    // Check if OPEN ticket exists for given sale and product
+     boolean existsBySale_SaleIdAndProduct_ProductIdAndStatusIn(Long saleId, Long productId, Iterable<Status> statuses);
 }
