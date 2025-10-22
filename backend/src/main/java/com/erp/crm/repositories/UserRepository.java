c647546c52cbc6195b1ac81fbd817a483bf2c257
sparsh-15 <Himank@learning-crew.com>
2025-10-22 13:09:50 +0530

    update UserRepository

--- backend/src/main/java/com/erp/crm/repositories/UserRepository.java
+++ backend/src/main/java/com/erp/crm/repositories/UserRepository.java
@@ -4,12 +4,22 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.erp.crm.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
     List<User> findByName(String name);
+
     List<User> findByUserIdGreaterThanEqualOrderByUserIdDesc(Long id);
+
     List<User> findByRoleName(String roleName);
+
+    @Query("SELECT u.role.name, COUNT(u) FROM User u GROUP BY u.role.name")
+    List<Object[]> getUserCountByRole();
+
+    @Query("SELECT u.role.name, COUNT(u) FROM User u WHERE u.isActive = true GROUP BY u.role.name")
+    List<Object[]> getActiveUserCountByRole();
 }
