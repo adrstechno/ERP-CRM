fba167de78e9d65983c0b45406d54b2f9c2ab1fa
sparsh-15 <Himank@learning-crew.com>
2025-10-22 13:08:57 +0530

    fix expense Repository

--- backend/src/main/java/com/erp/crm/repositories/ExpenseRepository.java
+++ backend/src/main/java/com/erp/crm/repositories/ExpenseRepository.java
@@ -1,12 +1,17 @@
 package com.erp.crm.repositories;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.erp.crm.models.Expense;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     List<Expense> findAllByUser_userId(Long userId);
+
+    @Query("SELECT SUM(e.amount) FROM Expense e WHERE e.expenseDate BETWEEN :start AND :end")
+    Double getTotalExpenses(LocalDate start, LocalDate end);
 }
