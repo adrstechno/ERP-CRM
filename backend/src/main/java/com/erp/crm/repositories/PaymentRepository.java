e56c7bec0a8dd148e33d5834d7ce26ce86e56738
sparsh-15 <Himank@learning-crew.com>
2025-10-22 13:09:18 +0530

    update payment repo

--- backend/src/main/java/com/erp/crm/repositories/PaymentRepository.java
+++ backend/src/main/java/com/erp/crm/repositories/PaymentRepository.java
@@ -4,7 +4,10 @@ import com.erp.crm.models.Payment;
 import com.erp.crm.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -12,4 +15,10 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByInvoice_InvoiceId(Long invoiceId);
 
     List<Payment> findByReceivedBy(User receivedBy);
+
+    @Query("SELECT SUM(p.amount) FROM Payment p WHERE p.paymentDate BETWEEN :start AND :end")
+    Double getTotalPayments(LocalDate start, LocalDate end);
+
+    @Query("SELECT SUM(i.totalAmount) - SUM(COALESCE(p.amount,0)) FROM Invoice i LEFT JOIN i.payments p WHERE i.invoiceDate BETWEEN :start AND :end")
+    Double getOutstandingPayments(LocalDate start, LocalDate end);
 }
