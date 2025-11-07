4c0217ec654acf175e2626c9d01c02e097335295
SakHack <sakshi2408jain@gmail.com>
2025-11-07 17:58:08 +0530

    feat: add CategoryStatDTO class for category statistics representation

---
+++ backend/src/main/java/com/erp/crm/dto/CategoryStatDTO.java
@@ -0,0 +1,13 @@
+package com.erp.crm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryStatDTO {
+    private String name;
+    private Long value;
+}
